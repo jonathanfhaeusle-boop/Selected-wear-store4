29d9a68ee2db19972a5b6b52ace9e4bdf3e6c06d
jonathanfhaeusle-boop <jonathan.f.haeusle@gmail.com>
2026-04-21 18:06:41 +0100

    Create products.ts

---
+++ data/products.ts
@@ -0,0 +1,34 @@
+export const products = [
+  {
+    id: 1,
+    name: "SW Hoodie Tech Pack",
+    price: "€79",
+    image: "/images/hoodie-techpack.jpeg",
+    tag: "Featured",
+    subtitle: "Original Hoodie Design",
+  },
+  {
+    id: 2,
+    name: "SELECTEDWEAR Hoodie",
+    price: "€79",
+    image: "/images/selectedwear-logo.png",
+    tag: "Logo",
+    subtitle: "Clean oversized fit",
+  },
+  {
+    id: 3,
+    name: "SELECTEDWEAR T-Shirt",
+    price: "€39",
+    image: "/images/selectedwear-logo.png",
+    tag: "Logo",
+    subtitle: "Everyday essential",
+  },
+  {
+    id: 4,
+    name: "SELECTEDWEAR Jacket",
+    price: "€119",
+    image: "/images/selectedwear-logo.png",
+    tag: "Logo",
+    subtitle: "Minimal outerwear",
+  },
+];
