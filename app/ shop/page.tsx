776f05e0923a855d308e3ae6f4135b669e6ec529
jonathanfhaeusle-boop <jonathan.f.haeusle@gmail.com>
2026-04-21 19:50:34 +0100

    Update page.tsx

--- app/ shop/page.tsx
+++ app/ shop/page.tsx
@@ -1,19 +1,4 @@
-import { ProductCard } from "../../components/ProductCard";
-import { products } from "../../data/products";
-
 export default function ShopPage() {
-  return (
-    <main className="section">
-      <div className="container">
-        <p className="eyebrow">Shop</p>
-        <h1 className="page-title">Alle Produkte</h1>
-
-        <div className="product-grid">
-          {products.map((product) => (
-            <ProductCard key={product.id} product={product} />
-          ))}
-        </div>
-      </div>
-    </main>
-  );
+  return <h1>SHOP TEST</h1>;
+}
 }
