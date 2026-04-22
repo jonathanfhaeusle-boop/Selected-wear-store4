af5c56f7c0f5da83e527940ea4b9b21d22b32d85
jonathanfhaeusle-boop <jonathan.f.haeusle@gmail.com>
2026-04-22 07:25:44 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,15 @@
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <main>
-      <h1>TEST</h1>
+<Image
+  src="/images/selectedwear-logo.jpeg"
+  alt="Selectedwear Logo"
+  width={300}
+  height={300}
+/>
 
-      <Image
-        src="/images/selectedwear-logo.jpg"
-        alt="Logo"
-        width={300}
-        height={300}
-      />
-
-      <Image
-        src="/images/hoodie-techpack.jpg"
-        alt="Hoodie"
-        width={500}
-        height={500}
-      />
-    </main>
-  );
-}
+<Image
+  src="/images/hoodie-techpack.jpeg"
+  alt="Hoodie Tech Pack"
+  width={500}
+  height={500}
+/>
